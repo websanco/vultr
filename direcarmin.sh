47a20aad250bb0bd95bbcf0956b2ce33b7ce0912
websanco <32726791+websanco@users.noreply.github.com>
2017-10-15 09:52:22 +0700

    Create direcarmin.sh

---
+++ direcarmin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#intall
+yum install wget gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx db4-devel
+wget http://www.directadmin.com/setup.sh  
+chmod 755 setup.sh  
+./setup.sh
